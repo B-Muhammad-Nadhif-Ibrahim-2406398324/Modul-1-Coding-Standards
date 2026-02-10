0ce83b98976acad7dbdb1dd3490299b0a18ab8d9
Nadhif Ibrahim <mnadhifibr@email.com>
2026-02-10 11:57:36 +0700

    add find product by id and update product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,14 +6,35 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
+    }
+
+    public Product findById(String id) {
+        return productData.stream()
+                .filter(p -> p.getProductId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Product update(Product updatedProduct) {
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(updatedProduct.getProductId())) {
+                productData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;
     }
 
     public Iterator<Product> findAll() {
